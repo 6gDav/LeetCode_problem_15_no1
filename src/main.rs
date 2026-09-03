efde745500b80b461afb55e3071cb4441885552e
6gDav <c6gdavprog0402@gmail.com>
2026-09-03 14:32:30 +0200

    feat: early return added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod solution;
+
 use solution::Solution as sol;
 
 fn main() {
-    let s =  vec![-1,0,1,2,-1,-4];
-    let _ = sol::three_sum(s);
+    let s =  vec![1,2,4,5,6,7];
+    let res = sol::three_sum(s);
+    println!("{:?}", res)
 }
--- src/solution.rs
+++ src/solution.rs
@@ -2,6 +2,27 @@ pub struct Solution;
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-       std::unimplemented!()
+        let mut res: Vec<Vec<i32>> = vec![];
+
+        let mut cont_minus = false;
+        let mut count_zero = 0;
+
+        for el in nums.iter() {
+            if el < &0 {
+                cont_minus = true;
+            }
+            if el == &0 {
+                count_zero = count_zero + 1;
+            }
+        }
+
+        if count_zero == nums.len() {
+            return vec![nums];
+        }
+
+        if !cont_minus {
+            return  res;
+        }  
+        res
     }
 }
