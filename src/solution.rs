8249fe4af67c6c384064d6c94340e4d6cda60192
6gDav <c6gdavprog0402@gmail.com>
2026-09-03 16:43:50 +0200

    feat: algortihm fixed

--- src/solution.rs
+++ src/solution.rs
@@ -6,33 +6,18 @@ impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut res: Vec<Vec<i32>> = vec![];
 
-        let mut cont_minus = false;
-        let mut count_zero = 0;
-
-        for el in nums.iter() {
-            if el < &0 {
-                cont_minus = true;
-            }
-            if el == &0 {
-                count_zero = count_zero + 1;
-            }
-        }
-
-        if count_zero == nums.len() {
-            return vec![nums];
-        }
-
-        if !cont_minus {
+        if nums.len() < 3 {
             return res;
         }
-
-        let mut dummy_nums = nums.clone();
+        
+        let dummy_nums = nums.clone();
 
         for ind in 0..dummy_nums.len() {
-            for ind2 in 1..dummy_nums.len() {
-                for ind3 in 1..dummy_nums.len() {
+            for ind2 in (ind + 1)..dummy_nums.len() {
+                for ind3 in (ind + 2)..dummy_nums.len() {
                     if dummy_nums[ind] + dummy_nums[ind2] + dummy_nums[ind3] == 0 {
-                        let new_res_el = vec![dummy_nums[ind], dummy_nums[ind2], dummy_nums[ind3]];
+                        let mut new_res_el = vec![dummy_nums[ind], dummy_nums[ind2], dummy_nums[ind3]];
+                        new_res_el.sort();
 
                         if !res.contains(&new_res_el) {
                             res.push(new_res_el);
